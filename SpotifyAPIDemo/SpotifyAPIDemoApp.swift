2e0aba6fbf7da4043f3224e1a0bb3f9410a7d207
Kunal <bajajku@sheridancollege.ca>
2024-11-03 16:21:30 -0500

    Initial Commit

---
+++ SpotifyAPIDemo/SpotifyAPIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyAPIDemoApp.swift
+//  SpotifyAPIDemo
+//
+//  Created by Kunal Bajaj on 2024-11-03.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyAPIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpotifyAPIDemoTests/SpotifyAPIDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyAPIDemoTests.swift
+//  SpotifyAPIDemoTests
+//
+//  Created by Kunal Bajaj on 2024-11-03.
+//
+
+import Testing
+@testable import SpotifyAPIDemo
+
+struct SpotifyAPIDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
